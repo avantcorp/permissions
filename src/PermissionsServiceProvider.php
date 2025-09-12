7bf8de79fe0a5486653ed052939d59c131edfa86
Ali Saleem <alisaleem@outlook.com>
2025-09-12 16:45:42 +0100

    v2

--- src/PermissionsServiceProvider.php
+++ src/PermissionsServiceProvider.php
@@ -27,41 +27,5 @@ class PermissionsServiceProvider extends ServiceProvider
         Gate::after(function (Authorizable $authorizable, $ability, $result, $arguments): bool {
             return !is_null($result) ? $result : $authorizable->hasRole(Permission::SUPERUSER);
         });
-
-        Gate::guessPolicyNamesUsing(function ($class) {
-            $classDirname = str_replace('/', '\\', dirname(str_replace('\\', '/', $class)));
-
-            $classDirnameSegments = explode('\\', $classDirname);
-
-            if (str_starts_with($classDirname, 'App\Policies')) {
-                return $class;
-            }
-
-            return Arr::wrap(Collection::times(
-                count($classDirnameSegments),
-                function ($index) use ($class, $classDirnameSegments) {
-                    $classDirname = implode('\\', array_slice($classDirnameSegments, 0, $index));
-
-                    return $classDirname.'\\Policies\\'.class_basename($class).'Policy';
-                }
-            )->when(str_contains($classDirname, '\\Models\\'), function ($collection) use ($class, $classDirname) {
-                return $collection->concat([
-                    str_replace(
-                        '\\Models\\',
-                        '\\Policies\\',
-                        $classDirname
-                    ).'\\'.class_basename($class).'Policy',
-                ])
-                    ->concat([
-                        str_replace(
-                            '\\Models\\',
-                            '\\Models\\Policies\\',
-                            $classDirname
-                        ).'\\'.class_basename($class).'Policy',
-                    ]);
-            })->reverse()->values()->first(function ($class) {
-                return class_exists($class);
-            }) ?: [$classDirname.'\\Policies\\'.class_basename($class).'Policy']);
-        });
     }
 }
